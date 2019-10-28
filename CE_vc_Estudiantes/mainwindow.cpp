c10dc3a39ff2557f7272b512e872f199d9473a35
Hazel <hazel.martilo@gmail.com>
2019-10-27 20:07:01 -0600

    Mapeo de la matriz

--- CE_vc_Estudiantes/mainwindow.cpp
+++ CE_vc_Estudiantes/mainwindow.cpp
@@ -25,8 +25,8 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
     int shiftx=62,shifty=29;
     int x=MapeadorX(cursor->pos().x()),y= MapeadorY(cursor->pos().y());
     int mapa_x = Definicion_MapaX(cursor->pos().x()), mapa_y= Definicion_MapaY(cursor->pos().y());
-    cout<<"Posición X:"<<cursor->pos().x() <<" Posición Y:"<<cursor->pos().y()<<endl;
-    cout<<"LA POSICION PRESIONADA ES: ("<<mapa_x<<" , "<<mapa_y<< " )"<<endl;
+    //cout<<"Posición X:"<<cursor->pos().x() <<" Posición Y:"<<cursor->pos().y()<<endl;
+    //cout<<"LA POSICION PRESIONADA ES: ("<<mapa_x<<" , "<<mapa_y<< " )"<<endl;
     ColocarImagen(x,y);
 
 
@@ -76,40 +76,59 @@ MainWindow::~MainWindow()
 
 void MainWindow::InicializarMatriz()
 {
-    Personaje  *cualquierPernaje=new Personaje;
-    cualquierPernaje->id=-1;
+    for(int i = 0; i < 10; i++){ //despues del primer for crea la lista
         Lista <Personaje> *lista=new Lista<Personaje>();
-        // 10 elementos de una fila
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        lista->add_end(*cualquierPernaje);
-        //10 filas y así se forma la matriz de 10xx10
 
-        this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
-         this->matriz->Append(lista);
+        for(int j = 0; j < 10; j++){
+            Personaje  *cualquierPernaje=new Personaje;
+            cualquierPernaje->id=-1; //para indicar que esta vacio
+            lista->add_end(*cualquierPernaje);
+        }
+        this->matriz->Append(lista); // despues de 10 elementos en una lista se mete en la matriz
+    }
+//    Personaje  *cualquierPernaje=new Personaje;
+//    cualquierPernaje->id=-1;
+//        Lista <Personaje> *lista=new Lista<Personaje>();
+//        // 10 elementos de una fila
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        lista->add_end(*cualquierPernaje);
+//        //10 filas y así se forma la matriz de 10xx10
+
+//        this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
+//         this->matriz->Append(lista);
 }
 
 void MainWindow::ColocarImagen(int x, int y)
 {
-    Personaje *personaje;
+    Personaje* personaje;
+    int posX_torre = Definicion_MapaY(y);
+    int posY_torre = Definicion_MapaX(x);
+//    int posX_torre = Definicion_MapaX(cursor->pos().x());
+//    int posY_torre= Definicion_MapaY(cursor->pos().y());
+
     if (this->clickedPersonaje=="arquero11.png"){
-        Arquero* arquero=new Arquero();
+        Arquero *arquero=new Arquero();
         personaje=arquero;
+
+
+        cout<<"objeto posicionado en: "<<posX_torre<<" , "<< posY_torre<<endl;
+        //this->matriz->EstablecerElemento(posX_torre, posY_torre, personaje);
     }
     else if(this->clickedPersonaje=="mago11.png"){
         // crear un objeto de mago
@@ -136,7 +155,8 @@ void MainWindow::ColocarImagen(int x, int y)
     imagen->show();
     this->clickedPersonaje="";
     }
-
+    this->matriz->EstablecerElemento(posX_torre, posY_torre, *personaje);
+    this->matriz->Display();
 
     cout<<"poniendo imagen";
 
@@ -245,70 +265,71 @@ void MainWindow::ColocarImagen(int x, int y)
 
 int MainWindow::Definicion_MapaX(int mapa_x){
     int posX=0;
-    if(mapa_x >=243 && mapa_x <=388){
+    mapa_x = mapa_x + 48;
+    if(mapa_x ==243){
         posX = 0;
     }
-    else if(mapa_x<=563 && mapa_x>=388){
+    else if(mapa_x == 388){
         posX = 1;
     }
-    else if(mapa_x<=722 && mapa_x>=563){
+    else if(mapa_x == 563){
         posX = 2;
     }
-    else if(mapa_x<=878 && mapa_x>=722){
+    else if(mapa_x == 722){
         posX = 3;
     }
-    else if(mapa_x<=1036 && mapa_x>=878){
+    else if(mapa_x == 878){
         posX = 4;
     }
-    else if(mapa_x<=1198 && mapa_x>=1036){
+    else if(mapa_x == 1036){
         posX = 5;
     }
-    else if(mapa_x<=1358 && mapa_x>=1198){
+    else if(mapa_x == 1198){
         posX = 6;
     }
-    else if(mapa_x<=1509 && mapa_x>=1358){
+    else if(mapa_x == 1358){
         posX = 7;
     }
-    else if(mapa_x<=1679 && mapa_x>=1509){
+    else if(mapa_x == 1509){
         posX = 8;
     }
-    else if(mapa_x<=1800 && mapa_x>=1679){
+    else if(mapa_x == 1679){
         posX = 9;
     }
-
     return posX;
 }
 
 int MainWindow::Definicion_MapaY(int mapa_y){
     int posY=0;
-    if (mapa_y>=120 && mapa_y<=207){
+    mapa_y = mapa_y + 45;
+    if (mapa_y==120 ){
         posY=0;
     }
-    else if (mapa_y<=281 && mapa_y>=207){
+    else if (mapa_y == 207){
         posY=1;
     }
-    else if (mapa_y<=368 && mapa_y>=281){
+    else if (mapa_y == 281){
         posY=2;
     }
-    else if (mapa_y<=453 && mapa_y>=368){
+    else if (mapa_y == 368){
         posY=3;
     }
-    else if (mapa_y<=534 && mapa_y>=453){
+    else if (mapa_y == 453){
         posY=4;
     }
-    else if (mapa_y<=619 && mapa_y>=534){
+    else if (mapa_y == 534){
         posY=5;
     }
-    else if (mapa_y<=713 && mapa_y>=619){
+    else if (mapa_y == 619){
         posY=6;
     }
-    else if (mapa_y<=803 && mapa_y>=713){
+    else if (mapa_y == 713){
         posY=7;
     }
-    else if (mapa_y<=893 && mapa_y>=803){
+    else if (mapa_y == 803){
         posY=8;
     }
-    else if (mapa_y<=973 && mapa_y>=893){
+    else if (mapa_y == 893){
         posY=9;
     }
     return posY;
@@ -321,41 +342,41 @@ void MainWindow::ColocarPersonajeMatriz(int x, int y, Personaje personaje)
 
 
 void MainWindow::draw_matriz(){
-    Personaje p;
-    Lista<Enemigo> enemigos;
-    int num;
+//    Personaje p;
+//    Lista<Enemigo> enemigos;
+//    int num;
 
-    //prueba de la matriz
-    Matriz <Personaje> *matriz=new Matriz<Personaje>();
+//    //prueba de la matriz
+//    Matriz <Personaje> *matriz=new Matriz<Personaje>();
 
-    Personaje  *cualquierPernaje=new Personaje();
-    Lista <Personaje> *lista=new Lista<Personaje>();
-    // 10 elementos de una fila
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    lista->add_end(*cualquierPernaje);
-    //10 filas y así se forma la matriz de 10xx10
+//    Personaje  *cualquierPernaje=new Personaje();
+//    Lista <Personaje> *lista=new Lista<Personaje>();
+//    // 10 elementos de una fila
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    lista->add_end(*cualquierPernaje);
+//    //10 filas y así se forma la matriz de 10xx10
 
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
-    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
+//    matriz->Append(lista);
 
-    matriz->Display();
-    // finalización de la prueba de la matriz
+//    matriz->Display();
+//    // finalización de la prueba de la matriz
 
 }
 
